eeba5c8999c3723d4cf5b282a0138e5182edebd9
zhangsan <zhangsan@itcast.cn>
2018-09-13 19:17:26 +0800

    ishop第一次版本提交 测试sellergoods-service成功;测试manager-web成功.可以访问到分页页面.

---
+++ ishop-dao/src/main/java/com/ishop/mapper/TbCitiesMapper.java
@@ -0,0 +1,30 @@
+package com.ishop.mapper;
+
+import com.ishop.pojo.TbCities;
+import com.ishop.pojo.TbCitiesExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbCitiesMapper {
+    int countByExample(TbCitiesExample example);
+
+    int deleteByExample(TbCitiesExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TbCities record);
+
+    int insertSelective(TbCities record);
+
+    List<TbCities> selectByExample(TbCitiesExample example);
+
+    TbCities selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") TbCities record, @Param("example") TbCitiesExample example);
+
+    int updateByExample(@Param("record") TbCities record, @Param("example") TbCitiesExample example);
+
+    int updateByPrimaryKeySelective(TbCities record);
+
+    int updateByPrimaryKey(TbCities record);
+}
